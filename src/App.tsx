5a106fcbbfaa9a309e55f52d5fad82e3bf9ac00d
diffshare <diffshare@gmail.com>
2023-03-31 21:02:42 +0900

    chatgptにrequestするように実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {ChatCompletionRequestMessageRoleEnum, Configuration, CreateChatCompletionRequest, OpenAIApi} from 'openai';
 
 function App() {
+  const [apiKey, setApiKey] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [response, setResponse] = useState('');
+
+  const handleSend = async () => {
+    if (!userInput || !apiKey) return;
+
+    const configuration = new Configuration({
+      apiKey
+    });
+    const openai = new OpenAIApi(configuration);
+
+    const requestOptions: CreateChatCompletionRequest = {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: ChatCompletionRequestMessageRoleEnum.User,
+          content: `User: ${userInput}\nAssistant:`,
+        }
+      ],
+      max_tokens: 150,
+      n: 1,
+      temperature: 0.8,
+    };
+
+    try {
+      const result = await openai.createChatCompletion(requestOptions);
+      const generatedResponse = result?.data.choices[0].message?.content;
+      setResponse(`Assistant: ${generatedResponse}`);
+    } catch (error) {
+      console.error(error);
+      setResponse('Error: Failed to get a response from ChatGPT.');
+    }
+
+    setUserInput('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ChatGPT Electron App</h1>
+      <label htmlFor="api-key">API Key:</label>
+      <input
+        id="api-key"
+        type="text"
+        value={apiKey}
+        onChange={(e) => setApiKey(e.target.value)}
+        placeholder="Enter your OpenAI API key"
+      />
+      <input
+        type="text"
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+        placeholder="Type your message here..."
+      />
+      <button onClick={handleSend}>Send</button>
+      <div>{response}</div>
     </div>
   );
 }
---
+++ src/electron.js
@@ -0,0 +1,26 @@
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+function createWindow() {
+    var win = new electron_1.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+    win.loadURL("http://localhost:3000");
+    //   win.loadFile(path.join(__dirname, '../public/index.html'));
+}
+electron_1.app.whenReady().then(createWindow);
+electron_1.app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        electron_1.app.quit();
+    }
+});
+electron_1.app.on('activate', function () {
+    if (electron_1.BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
